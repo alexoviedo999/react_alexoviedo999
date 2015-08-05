16658de38a38b619d0f91c269878d9c90b0a31e7
alexoviedo <alexoviedo999@gmail.com>
2015-08-05 11:52:11 -0400

    added cards to index.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,13 @@
 
 import React from 'react'
 var Router = require('react-router')
-import About from './About.jsx'
 var Link = Router.Link
 import { LinkBtn } from 'rebass'
 
 class Header extends React.Component {
 
   render () {
-    let headerClass = 'flex flex-center flex-wrap mxn2'
+    let headerClass = 'flex flex-center flex-wrap mxn2 p2'
     let buttonClass = 'button rounded button-transparent '
     buttonClass += this.props.light ? 'black' : 'bg-darken-2'
     headerClass += this.props.light ? ' black' : ' white'
@@ -16,16 +15,16 @@ class Header extends React.Component {
     return (
       <header className={headerClass}>
         
-        <div className='flex-auto center px2 mb2'>
+        <div className='flex-auto px2 mb2'>
           <h1 className='m0 headline'>
             {this.props.name}
-            <span className='h4'> v{this.props.version}</span>
+            {/* <span className='h4'> v{this.props.version}</span> */}
           </h1>
-          <p className='m0'>{this.props.description}</p>
+          <p className='m0 right bold'>{this.props.description}</p> 
           <div className="left">
           <h4>
-            <span className="m3"><a href="/" class="btn">Home</a></span>
-            <span><a href="/about" class="btn">About Me</a></span>
+            <span><a href="/" className="btn">Home</a></span>
+            <span><a href="/about" className="btn">About Me</a></span>
             
           </h4>
 
--- components/Index.jsx
+++ components/Index.jsx
@@ -1,10 +1,79 @@
 
 var React = require('react')
+import { Row } from 'rebass'
+import { Col } from 'rebass'
+import { Card } from 'rebass'
+import { Media } from 'rebass'
+
 
 var Index = React.createClass({
   render: function () {
+
+  	let ReactElement1 = <img src='http://placehold.it/300x200&text=slide1'/>;
+    let ReactElement2 = <img src='http://placehold.it/40x40&text=slide1'/>;
+
     return (
-        <h1>home body</h1>
+    	<div>
+	        <h1>home body</h1>
+
+	        <Row>
+				<Col
+				sm={6}
+				md={4}>
+					<Card image={ReactElement1}>
+					  <Media
+					    middle={true}
+					    image={ReactElement2}>
+					    <h3 className="h4 m0">
+					      Nested Hamburger
+					    </h3>
+					  </Media>
+					</Card>
+				</Col>
+
+				<Col
+				sm={6}
+				md={4}>
+				<Card image={ReactElement1}>
+				  <Media
+				    middle={true}
+				    image={ReactElement2}>
+				    <h3 className="h4 m0">
+				      Nested Hamburger
+				    </h3>
+				  </Media>
+				</Card>
+				</Col>
+
+				<Col
+				sm={6}
+				md={4}>
+					<Card image={ReactElement1}>
+					  <Media
+					    middle={true}
+					    image={ReactElement2}>
+					    <h3 className="h4 m0">
+					      Nested Hamburger
+					    </h3>
+					  </Media>
+					</Card>
+				</Col>
+
+				<Col
+				sm={6}
+				md={4}>
+					<Card image={ReactElement1}>
+					  <Media
+					    middle={true}
+					    image={ReactElement2}>
+					    <h3 className="h4 m0">
+					      Nested Hamburger
+					    </h3>
+					  </Media>
+					</Card>
+				</Col>
+			</Row>
+		</div>
     )
   }
 })
